dcacb600bf9d08e73b43bd0c732b26c28bada284
eehwan <eehwan.shin@gmail.com>
2020-08-04 02:19:04 +0900

    working on LIS algorithm

---
+++ 2352.py
@@ -0,0 +1,14 @@
+import sys
+
+num = sys.stdin.readline()
+ports = list(map(int, sys.stdin.readline().split()))
+
+count = 1
+current_min = num
+current_max = 0
+new_ports =[0,]
+for i in range(num):
+    if port[i]<=new_ports[-1]
+        new_port[-1] = ports[i]
+    if port[i]>new_ports[-1] and port[i]<=current_max:
+        count+=1
