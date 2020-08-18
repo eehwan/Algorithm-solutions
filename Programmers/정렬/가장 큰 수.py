88427ed8b39f8eaee2ddb97c97021ebe6d487ba4
eehwan <eehwan.shin@gmail.com>
2020-08-19 00:17:32 +0900

    20-08-19

--- Programmers/정렬/가장 큰 수.py
+++ Programmers/정렬/가장 큰 수.py
@@ -15,15 +15,20 @@ def solution(numbers):
     numbers.sort(key =lambda x: (new_sort(x),len(x)), reverse=True)
     print(numbers)
     print(list(map(new_sort,numbers)))
-    return reduce(lambda x,y:x+y, numbers)
+    return str(int(reduce(lambda x,y:x+y, numbers,"")))
 
-from itertools import permutations as permut
-def solution2(numbers):
-    numbers = list(map(str, numbers))
-    array = []
-    for x in permut(numbers, len(numbers)):
-        array.append("".join(x))
-    return max(array)
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key =lambda x: x*3, reverse=True)
+#     return str(int("".join(numbers)))
+
+# from itertools import permutations as permut
+# def solution2(numbers):
+#     numbers = list(map(str, numbers))
+#     array = []
+#     for x in permut(numbers, len(numbers)):
+#         array.append("".join(x))
+#     return max(array)
 """
 [1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 """
@@ -31,6 +36,6 @@ def solution2(numbers):
 """
 [5,6,3]
 """
-
-print(solution([22,2,21,222,211,212,2112,2121,2122,2111]))
-print(solution2([22,2,21,222,211,212,2112,2121,2122,2111]))
+print(solution([0,0]))
+print(solution([96,966,9699,9696,69,6969,699,6966,6999]))
+print(solution2([96,966,9699,9696,69,6969,699,6966,6999]))
