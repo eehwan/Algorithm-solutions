0b50a4e3d85038f4ac44d34223a4072bc7616b5b
eehwan <eehwan.shin@gmail.com>
2020-08-27 23:07:14 +0900

    구명보트

---
+++ Programmers/탐욕법(Greedy)/구명보트.py
@@ -0,0 +1,23 @@
+def solution(people, limit):
+    answer = 0
+    return answer
+
+test_case =\
+"""
+"1924", 2
+"1231234", 3
+"4177252841", 4
+"123917415", 6
+"12398745", 5
+"""
+expected_value =\
+"""
+94
+"3234"
+"775841"
+"975"
+"987"
+"""
+
+
+print(solution("12398745", 5))
