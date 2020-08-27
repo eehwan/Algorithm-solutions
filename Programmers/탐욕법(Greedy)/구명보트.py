a68690c7b306ca5e9c1e50256750fb402e41ba86
eehwan <eehwan.shin@gmail.com>
2020-08-28 03:21:18 +0900

    complete 구명보트

--- Programmers/탐욕법(Greedy)/구명보트.py
+++ Programmers/탐욕법(Greedy)/구명보트.py
@@ -1,31 +1,43 @@
 def solution(people, limit):
-    people.sort()
-    weight_in_boat = 0
-    count_boat = 0
-    for peo in people:
-        if weight_in_boat == 0:
-            weight_in_boat = peo
-        elif weight_in_boat + peo <= limit:
+    people.sort(reverse=True)
+    # print(people, limit)
+    if len(people)<=1:
+        return len(people)
+    weight_in_boat, count_boat = 0, 0
+    i,j = 0,0
+    length = len(people)
+    while i+j< length:
+        weight_in_boat = people[i]
+        # print(i)
+        i+=1
+        if weight_in_boat + people[-j-1] <= limit:
             count_boat += 1
             weight_in_boat = 0
+            # print(-j-1)
+            j+=1
         else:
             count_boat += 1
-            weight_in_boat = peo
-        print(weight_in_boat, count_boat,limit)
-    return count_boat if weight_in_boat==0 else count_boat+1
+            weight_in_boat = 0
+    #     print(f"in_boat : {weight_in_boat}, count : {count_boat}, index : {i},{j}")
+    #     print("=================================")
+    # print("<last>")
+    # print(f"in_boat : {weight_in_boat}, count : {count_boat}, index : {i},{j}")
+    return count_boat
 
 test_case =\
 """
 [70, 50, 80, 50], 100
 [70, 80, 50], 100
 [40, 40, 90, 90, 100], 180
+[40, 40, 80, 90, 95, 110, 110], 150
 """
 expected_value =\
 """
 3
 3
 3
+5
 """
 
 
-print(solution([70, 50, 80, 50], 100))
+print(solution([70, 80, 50], 100))
