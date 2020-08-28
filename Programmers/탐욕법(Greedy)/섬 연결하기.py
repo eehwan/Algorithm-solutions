a544362d34e95e86e8a3c1de3dad640ac92fc249
eehwan <eehwan.shin@gmail.com>
2020-08-28 21:47:56 +0900

    섬 연결하기

--- Programmers/탐욕법(Greedy)/섬 연결하기.py
+++ Programmers/탐욕법(Greedy)/섬 연결하기.py
@@ -1,28 +1,46 @@
 def solution(n, costs):
     costs.sort(key=lambda x: x[2])
     print(costs)
-    visited_islands = set()
+    visited_islands = set([0])
     answer = 0
-    for cost in costs:
-        print(f"loop : {cost}")
-        if cost[0] in visited_islands and cost[1] in visited_islands:
-            continue
-        else:
-            visited_islands |= {cost[0], cost[1]}
-            answer += cost[2]
-            print("added")
-        if len(visited_islands) == n:
-            break
+    while len(visited_islands) != n:
+        for element in costs:
+            start, end, cost = element
+            if start in visited_islands or end in visited_islands:
+                if start in visited_islands and end in visited_islands:
+                    continue
+                else:
+                    visited_islands |= {start, end}
+                    answer += cost
+                    # print("added")
+                    # print(f"loop : {element}")
+                    # print(f"visited_islands : {visited_islands}")
+                    break
     return answer
+
 
 test_case =\
 """
 4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]
+5, [[0, 1, 1], [3, 1, 1], [0, 2, 2], [0, 3, 2], [0, 4, 100]]
+6, [[0, 1, 5], [0, 3, 2], [0, 4, 3], [1, 4, 1], [3, 4, 10], [1, 2, 2], [2, 5, 3], [4, 5, 4]]
+4, [[0, 1, 5], [1, 2, 3], [2, 3, 3], [3, 1, 2], [3, 0, 4]]
+5, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 3], [2, 3, 8], [3, 4, 1]]
+5, [[0, 1, 1], [3, 4, 1], [1, 2, 2], [2, 3, 4]]
+4, [[2, 3, 3], [2, 4, 4], [3, 4, 7], [3, 5, 3], [4, 5, 10]]
+5, [[0,1,1],[0,2,2],[1,2,5],[1,3,3],[2,3,8],[3,4,1]]
 """
 expected_value =\
 """
 4
+104
+11
+9
+7
+8
+10
+7
 """
 
-
-print(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]))
+answer = solution(6, [[0, 1, 5], [0, 3, 2], [0, 4, 3], [1, 4, 1], [3, 4, 10], [1, 2, 2], [2, 5, 3], [4, 5, 4]])
+print(answer)
