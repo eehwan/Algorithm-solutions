726f90cc6436c755bda4775eba6207333749b523
eehwan <eehwan.shin@gmail.com>
2020-08-30 23:47:13 +0900

    정수 삼각형

---
+++ Programmers/동적계획법(Dynamic Programming)/정수 삼각형.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    answer = 0
+    return answer
+
+test_case =\
+"""
+[[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+"""
+expected_value =\
+"""
+30
+"""
+
+print(solution(\
+[[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]\
+))
