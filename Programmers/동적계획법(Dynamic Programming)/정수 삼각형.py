d7d0e52408cc80c928aaee011106e65b26a3ce3c
eehwan <eehwan.shin@gmail.com>
2020-08-31 23:23:19 +0900

    정수 삼각형 timeout

--- Programmers/동적계획법(Dynamic Programming)/정수 삼각형.py
+++ Programmers/동적계획법(Dynamic Programming)/정수 삼각형.py
@@ -1,6 +1,23 @@
-def solution(triangle):
-    answer = 0
+def solve(triangle,m,n):
+    now = triangle[m][n]
+    # print(m,n,now)
+    if m == 0:
+        return now
+    if n == 0:
+        answer = solve(triangle,m-1,n) + now
+        # print(answer)
+        return answer
+    if n == len(triangle[m])-1:
+        answer = solve(triangle,m-1,n-1) + now
+        # print(answer)
+        return answer
+    answer = max(solve(triangle,m-1,n-1),solve(triangle,m-1,n)) + now
+    # print(answer)
     return answer
+def solution(triangle):
+    m = len(triangle)
+    n = len(triangle[-1])
+    return max(solve(triangle,m-1,i) for i in range(n))
 
 test_case =\
 """
