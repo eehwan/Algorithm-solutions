ec880c9072a1829b46a125b5dcfbea6fb3b6f8bf
eehwan <eehwan.shin@gmail.com>
2020-10-30 03:16:16 +0900

    타겟 넘버

--- Programmers/깊이너비우선탐색/타겟 넘버.py
+++ Programmers/깊이너비우선탐색/타겟 넘버.py
@@ -15,3 +15,11 @@ def solution(numbers, target):
     if remain == 0:
         return 1
     return func(numbers).count(target)
+
+# def solution(numbers, target):
+#     if not numbers and target == 0 :
+#         return 1
+#     elif not numbers:
+#         return 0
+#     else:
+#         return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
