21b3c801eba3e8753514b5cb9199074fad32d5de
eehwan <eehwan.shin@gmail.com>
2020-11-05 22:37:38 +0900

    단어 변환 - still error

--- Programmers/깊이너비우선탐색/단어 변환.py
+++ Programmers/깊이너비우선탐색/단어 변환.py
@@ -1,21 +1,29 @@
-def func(word1, word2):
+def isConvertable(word1, word2):
     match_num = 0
     for a,b in zip(word1, word2):
         if a == b:
             match_num += 1
-    print("func\n", word1, word2, match_num)
-    return match_num
+    return True if match_num == 2 else False
+def dfs(begin, target, words, visited, count):
+    stacks = [begin]
+    while stacks:
+        stack = stacks.pop()
+        if isConvertable(stack, target):
+            return count
+        for i in range(len(words)):
+            if isConvertable(stack, words[i]):
+                if visited[i] == 1:
+                    continue
+                visited[i] = 1
+                stacks.append(words[i])
+                print(words[i])
+        count += 1
+    return count
+
 def solution(begin, target, words, count = 0):
-    print("=======================================")
-    print("start\n", begin, target, words, count)
     if target not in words:
         return 0
-    if func(begin, target) == len(target) - 1:
-        return count + 1
-    for next_begin in words:
-        print("\nchechking words:\n", begin, next_begin, "\n")
-        if func(begin, next_begin) == 2 and func(next_begin, target) > func(begin, target):
-                print(next_begin, words[1:], count+1)
-                return solution(next_begin, target, words[1:], count+1)
+    visited = [0 for i in words]
+    return dfs(begin, target, words, visited, 0)
 
 print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
