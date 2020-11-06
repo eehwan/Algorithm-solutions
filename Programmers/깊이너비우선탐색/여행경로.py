e435bdb506ab8c1a93faf88e5f4fafa6ebfa620e
eehwan <eehwan.shin@gmail.com>
2020-11-07 00:23:52 +0900

    여행경로

--- Programmers/깊이너비우선탐색/여행경로.py
+++ Programmers/깊이너비우선탐색/여행경로.py
@@ -3,10 +3,24 @@ def solution(tickets):
         for i in range(len(tickets)):
             if used[i]:
                 continue
-            if tickets[i][0] == current:
+            if tickets[i][0] != current:
+                continue
+            # 길이 끊기지 아니한 지 확인
+            canVisit = False
+            for j in range(len(tickets)):
+                if used[j]:
+                    continue
+                # 이후에 길을 이을 수 있는지
+                if tickets[i][1] == tickets[j][0]:
+                    canVisit = True
+                # 아니면 이 케이스가 맨 마지막 나머지인지
+                elif len(used) - sum(used) == 1:
+                    canVisit = True
+            if canVisit:
                 used[i] = 1
                 result.append(tickets[i][1])
-                return(dfs(tickets, tickets[i][1], used))
+                print(tickets, used)
+                return dfs(tickets, tickets[i][1], used)
 
     tickets.sort(key=lambda x: x[1])
     result = ["ICN"]
@@ -14,4 +28,4 @@ def solution(tickets):
     dfs(tickets, "ICN", used)
     return result
 
-print(solution([["ICN", "A"], ["A", "C"], ["A", "D"], ["D", "B"], ["B", "A"]]))
+print(solution([["ICN", "A"], ["ICN", "B"], ["B", "K"], ["K", "B"], ["B", "ICN"]]))
