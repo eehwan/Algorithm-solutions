970650c5b3542b0b3cd8049b41d56bc2b88e2006
eehwan <eehwan.shin@gmail.com>
2020-11-13 20:19:43 +0900

    단어 변환

--- Programmers/깊이너비우선탐색/단어 변환.py
+++ Programmers/깊이너비우선탐색/단어 변환.py
@@ -2,7 +2,7 @@ from collections import deque
 
 def isConvertable(word1, word2):
     unmatch = sum(a != b for a,b in zip(word1, word2))
-    return True if unmatch == 1 else False
+    return unmatch == 1
 
 def solution(begin, target, words):
     if target not in words:
@@ -18,13 +18,13 @@ def solution(begin, target, words):
             if visited[word] != 0:
                 continue
             if isConvertable(current, word):
-                print(word)
+                # print(word)
                 queue.append(word)
                 try:
                     visited[word] = visited[current] + 1
                 except:
                     visited[word] = 1
-                print(visited)
-        print(queue)
+                # print(visited)
+        # print(queue)
     return 0
 print(solution("hit", "hhh", ["hht","hhh", "dod", "dog", "lot", "cog"]))
