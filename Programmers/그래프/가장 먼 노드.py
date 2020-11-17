acbcb5253c1fb40f1dbe9a3d7c56ff90a8f279d5
eehwan <eehwan.shin@gmail.com>
2020-11-17 18:53:00 +0900

    가장 먼 노드

--- Programmers/그래프/가장 먼 노드.py
+++ Programmers/그래프/가장 먼 노드.py
@@ -1,16 +1,22 @@
-from collections import deque
-
 def solution(n, edge):
-    queue = deque()
-    queue.append(1)
-    visited = {key:0 for key in edge}
+    queue, queue2 = [1], set()
+    node = {key:[] for key in range(1,n+1)}
+    for a, b in edge:
+        node[a].append(b)
+        node[b].append(a)
+    visited = {key:0 for key in range(2, n+1)}
+    visited[1] = 1
     while queue:
-        current = queue.popleft()
-        for a,b in range(1,n+1):
-            if visited(a) = 1:
-                continue
-            elif visited(b) = 1:
-                continue
-            else:
+        for current in queue:
+            print(queue)
+            for destination in node[current]:
+                if visited[destination] == 0:
+                    queue2.add(destination)
+                    visited[destination] = 1
+                print(current, destination, visited, queue2)
+        if queue2 == set():
+            break
+        queue, queue2 = list(queue2), set()
+    return len(queue)
 
-    return
+print(solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
