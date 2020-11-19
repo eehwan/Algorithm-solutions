d60cb2412a300df69c9a2e3da603cf3b6d3e89d0
eehwan <eehwan.shin@gmail.com>
2020-11-19 17:37:49 +0900

    순위

--- Programmers/그래프/순위.py
+++ Programmers/그래프/순위.py
@@ -1,7 +1,13 @@
 def solution(n, results):
-    wins, loses = {}, {}
-    for i in range(1,n+1):
-        wins[i] = set()
-        loses[i] = set()
-    answer = 0
-    return answer
+    wins, loses = {x:set() for x in range(1,n+1) }, {x:set() for x in range(1,n+1)}
+    for winner, loser in results:
+        wins[winner].add(loser)
+        loses[loser].add(winner)
+    for i in range(1, n+1):
+        for lose in loses[i]:
+            wins[lose].update(wins[i])
+        for win in wins[i]:
+            loses[win].update(loses[i])
+    return sum(len(wins[i]) + len(loses[i]) == n - 1 for i in range(1, n+1))
+
+print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
