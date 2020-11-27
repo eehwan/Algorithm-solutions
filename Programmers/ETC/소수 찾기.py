08a66a29b1fe56efa453d56ea3139de35b002a37
eehwan <eehwan.shin@gmail.com>
2020-11-28 00:41:12 +0900

    소수 찾기

--- Programmers/ETC/소수 찾기.py
+++ Programmers/ETC/소수 찾기.py
@@ -1,15 +1,22 @@
-def isPrimeNum(k):
-    if k < 2:
-        return False
-    if k == 2 or k == 3:
-        return True
-    for i in range(2, int(k**0.5 + 2)):
-        if k % i == 0:
-            return False
-    return True
+# def isPrimeNum(k):
+#     if k < 2:
+#         return False
+#     if k == 2 or k == 3:
+#         return True
+#     for i in range(2, int(k**0.5 + 2)):
+#         if k % i == 0:
+#             return False
+#     return True
+# def solution(n):
+#     answer = 0
+#     for i in range(1, n+1):
+#         if isPrimeNum(i):
+#             answer += 1
+#     return answer
 def solution(n):
-    answer = 0
-    for i in range(1, n+1):
-        if isPrimeNum(i):
-            answer += 1
-    return answer
+    nums = set(range(2, n+1))
+    for i in range(2, n+1):
+        nums -= set(range(2*i, n+1, i))
+    return len(nums)
+
+print(solution(int(input("n :"))))
