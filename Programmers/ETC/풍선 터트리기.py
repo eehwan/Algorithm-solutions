834592abe0590084d39464cc48a4161c801b47b0
eehwan <eehwan.shin@gmail.com>
2020-12-02 18:36:59 +0900

    풍선 터트리기

--- Programmers/ETC/풍선 터트리기.py
+++ Programmers/ETC/풍선 터트리기.py
@@ -1,5 +1,14 @@
+import sys
 def solution(a):
-    answer = 0
-    return answer
+    result = [False for _ in a]
+    minFront, minRear = sys.maxsize, sys.maxsize
+    for i in range(len(a)):
+        if a[i] < minFront:
+            minFront = a[i]
+            result[i] = True
+        if a[-1-i] < minRear:
+            minRear = a[-1-i]
+            result[-1-i] = True
+    return sum(result)
 
 print(solution([-16,27,65,-2,58,-92,-71,-68,-61,-33]))
