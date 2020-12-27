00ae86c50cb13903366bff8e7a207f5157e609cd
eehwan <eehwan.shin@gmail.com>
2020-12-27 23:23:01 +0900

    가장 긴 팰린드롬

--- Programmers/ETC/Level 3/가장 긴 팰린드롬.py
+++ Programmers/ETC/Level 3/가장 긴 팰린드롬.py
@@ -1,7 +1,11 @@
 def solution(s):
-    answer = 0
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
+    answer = 1
+    for i in range(0, len(s)):
+        for j in range(1, (len(s) - i)//2 + 1):
+            if s[i:i+j][::-1] == s[i+j+1: i + 2*j+1]:
+                if answer < 2*j + 1: answer = 2*j + 1
+            elif s[i:i+j][::-1] == s[i+j: i + 2*j]:
+                if answer < 2*j: answer = 2*j
     return answer
+
+print(solution("aa"))
