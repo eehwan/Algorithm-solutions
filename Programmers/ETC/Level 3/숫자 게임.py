d6c1e46c061df62a1caef8419cf95f7cf706be5f
eehwan <eehwan.shin@gmail.com>
2020-12-27 21:03:31 +0900

    숫자 게임

--- Programmers/ETC/Level 3/숫자 게임.py
+++ Programmers/ETC/Level 3/숫자 게임.py
@@ -1,3 +1,13 @@
 def solution(A, B):
+    A.sort(reverse=True); B.sort(reverse=True)
+    print(A, B)
     answer = 0
+    while A and B:
+        a = A.pop(); b = B.pop()
+        print(a,b)
+        while B and a >= b:
+            b = B.pop()
+        if a < b: answer +=1; print("a: ", a, "b: ", b)
     return answer
+
+print(solution([1,2,3,4,5], [2,2,2,2,5]))
