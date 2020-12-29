6c47081ebf94ce89bfcbba760c1818d131a836fa
eehwan <eehwan.shin@gmail.com>
2020-12-29 15:08:23 +0900

    배달

--- Programmers/ETC/Level 3/배달.py
+++ Programmers/ETC/Level 3/배달.py
@@ -11,15 +11,14 @@ def solution(N, road, K):
 
     que = deque(costs[1].keys())
     while que:
+        print("que: ", que)
         current = que.popleft()
         for next_start in costs.get(current):
             new_cost = costs[1][current] + costs[current][next_start]
-            # print(f"1 -> {current} -> {next_start}  == {new_cost}")
-            # print(costs[1].get(next_start, float('inf')) > new_cost)
+            # print(f"1 -> {current} -> {next_start}\nnew_cost == {new_cost}")
             if costs[1].get(next_start, float('inf')) > new_cost:
                 costs[1][next_start] = new_cost
                 que.append(next_start)
-    # print(costs[1])
     return sum(1 for s in costs[1].values() if s <= K)
 
 print(solution(6, [[1, 2, 1], [1, 3, 2], [2, 3, 2], [3, 4, 3], [3, 5, 2], [3, 5, 3], [5, 6, 1]], 4))
