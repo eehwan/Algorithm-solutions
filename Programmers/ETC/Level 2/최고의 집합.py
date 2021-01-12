3ddff11330edb381e17ad16da529e73fc5e83b8f
eehwan <eehwan.shin@gmail.com>
2021-01-13 00:24:19 +0900

    최고의 집합

--- Programmers/ETC/Level 2/최고의 집합.py
+++ Programmers/ETC/Level 2/최고의 집합.py
@@ -1,3 +1,11 @@
 def solution(n, s):
     answer = []
+    while n:
+        temp = s//n
+        if temp == 0:
+            return [-1]
+        answer.append(temp)
+        s -= temp
+        n -= 1
     return answer
+print(solution(3, 2))
