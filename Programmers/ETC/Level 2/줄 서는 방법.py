d72cdaaf502de46864bb64fbdbafb307752af000
eehwan <eehwan.shin@gmail.com>
2021-01-16 23:55:44 +0900

    줄 서는 방법

--- Programmers/ETC/Level 2/줄 서는 방법.py
+++ Programmers/ETC/Level 2/줄 서는 방법.py
@@ -1,3 +1,9 @@
+from itertools import permutations
 def solution(n, k):
-    answer = []
-    return answer
+    i = 0
+    for a, b, c in permutations([i for i in range(1, n+1)], n):
+        i += 1
+        if i == k:
+            return [a, b, c]
+
+print(solution(3, 5))
