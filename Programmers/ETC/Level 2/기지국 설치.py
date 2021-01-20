ea443dd1f9d44536379e821d595c161576505ae1
eehwan <eehwan.shin@gmail.com>
2021-01-20 18:17:15 +0900

    기지국 설치

--- Programmers/ETC/Level 2/기지국 설치.py
+++ Programmers/ETC/Level 2/기지국 설치.py
@@ -3,18 +3,15 @@ import math
 def solution(n, stations, w):
     answer = 0
     start = 0
-    for i in range(len(stations)):
-        left = stations[i] - w - 1
-        right = stations[i] + w - 1
-        if start >= left and start <= right:
-            start = right + 1
-            continue
-        answer += math.ceil((left - start) / (w * 2 + 1))        
+    for s in stations:
+        left = s - w - 1
+        right = s + w - 1
+        if start < left:
+            answer += math.ceil((left - start)/(w*2 + 1))
         start = right + 1
-    
-    if start < n:
-        answer += math.ceil((n - start) / (w * 2 + 1))
-    
+        if start > n:
+            return answer
+    answer += math.ceil((n - start)/(w*2 + 1))
     return answer
 
 print(solution(11, [4,11], 1))
