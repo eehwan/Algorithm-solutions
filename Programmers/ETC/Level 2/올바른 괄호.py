fa0fd53252f8ebaab1d73602bd949974789616ed
eehwan <eehwan.shin@gmail.com>
2021-03-02 00:18:24 +0900

    올바른 괄호

---
+++ Programmers/ETC/Level 2/올바른 괄호.py
@@ -0,0 +1,9 @@
+def solution(s):
+    queue = []
+    for i in s:
+        if i == "(":
+            queue.append("(")
+        else:
+            try: queue.pop()
+            except: return False
+    return len(queue)==0 and True or False
