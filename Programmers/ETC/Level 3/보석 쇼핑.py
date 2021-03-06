36261586ecb3d5288621c8fa0174655a9af32e26
eehwan <eehwan.shin@gmail.com>
2021-03-06 14:14:59 +0900

    보석 쇼핑

--- Programmers/ETC/Level 3/보석 쇼핑.py
+++ Programmers/ETC/Level 3/보석 쇼핑.py
@@ -1,31 +1,24 @@
 import heapq
 class Gem:
-    def __init__(self, gemType, num):
-        self.gemType = gemType
-        self.num = num
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.value = end  - start
     def __lt__(self, other):
         return self.value < other.value
     def __gt__(self , other):
         return self.value > other.value
-    def __hash__(self):
-        return self.num
-    def getType(self):
-        return self.gemType
-    def getNum(self):
-        return self.num
-    def changeNum(self, num):
-        self.num = num
-    
+    def __str__(self):
+        return f"[{self.start}, {self.end}]"
+    def getList(self):
+        return [self.start, self.end]
 def solution(gems):
-    typesCount = len(set(gems))
-    gemRecord = []
-    ans = []
-    for i, gemType in enumerate(gems):
-        if gemType in map(lambda x: x.getType(), gemRecord):
-            gemType = filter(lambda x: x.getNum != i + 1, gemType)
-        else:
-            heapq.heappush(gemRecord, Gem(gemType, i + 1))
-        if len(gemRecord) == typesCount:
-            temp = sorted(gemRecord.items(), key = lambda x: x[1])
-            ans.append([temp[0][1] + 1, temp[-1][1] + 1])
-    return sorted(ans, key = lambda x: x[1]-x[0])[0]
+    gemCnt = len(set(gems))
+    gemMap = {}
+    answer = []
+    for i, v in enumerate(gems):
+        gemMap[v] = i
+        if len(gemMap) == gemCnt:
+            temp = gemMap.values()
+            heapq.heappush(answer, Gem(min(temp)+1, max(temp)+1))
+    return answer[0].getList()
