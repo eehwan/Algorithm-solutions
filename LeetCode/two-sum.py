131e707dfa8fba3b88a09336d659cca826df444e
eehwan <eehwan.shin@gmail.com>
2021-03-11 21:04:28 +0900

    two-sum

--- LeetCode/two-sum.py
+++ LeetCode/two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list, target: int) -> list:
         length = len(nums)
         for i in range(length - 1):
             for j in range(i + 1, length):
                 if nums[i] + nums[j] == target:
                     return [i, j]
                 
-print(Solution.twoSum([1,2,3,4,5,6,7,8], 15))
+print(Solution().twoSum([1,2,3,4,5,6,7,8], 15))
