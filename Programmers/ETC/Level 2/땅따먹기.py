482760d409a74896fa5d6cfa4a04c1ee23312fcd
eehwan <eehwan.shin@gmail.com>
2021-03-29 13:29:02 +0900

    땅따먹기

--- Programmers/ETC/Level 2/땅따먹기.py
+++ Programmers/ETC/Level 2/땅따먹기.py
@@ -1,18 +1,12 @@
 import copy
 def solution(land):
-    answer = land[0]
+    # print(land)
+    dp = copy.deepcopy(land)
     for i in range(1, len(land)):
-        new_answer = land[i]
         for j in range(4):
-            temp = copy.deepcopy(answer)
-            temp.pop(j)
-            print(j , temp)
-            print(land[i][j])
-            new_answer[j] += max(temp)
-            print(new_answer)
-            print("----------------")
-        answer = new_answer
-        print(new_answer)
-    return max(answer)
+            for k in range(4):
+                if k != j:
+                    dp[i][j] = max(dp[i][j], land[i][j] + dp[i-1][k])
+    return max(dp[-1])
 
 print(solution([[1, 2, 3, 5], [5, 6, 7, 8], [4, 3, 2, 1]]))
