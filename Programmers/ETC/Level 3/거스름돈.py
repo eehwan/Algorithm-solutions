760dfa736e121544180aa812cf47b974e56d6c21
eehwan <eehwan.shin@gmail.com>
2021-04-01 23:18:12 +0900

    거스름돈

--- Programmers/ETC/Level 3/거스름돈.py
+++ Programmers/ETC/Level 3/거스름돈.py
@@ -1,3 +1,12 @@
 def solution(n, money):
-    answer = 0
-    return answer
+    dp = [[0 for _ in range(n+1)] for _ in range(len(money))]
+    dp[0][0] = 1
+    for i in range(money[0], n+1, money[0]):
+        dp[0][i] = 1
+    for y in range(1, len(money)):
+        for x in range(n+1):
+            if x >= money[y]:
+                dp[y][x] = dp[y-1][x] + dp[y][x - money[y]]
+            else:
+                dp[y][x] = dp[y-1][x]
+    return dp[-1][-1]
