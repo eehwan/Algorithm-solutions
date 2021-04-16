6074ddb257a48ec725da83cd9c8f7dec44895c63
eehwan <eehwan.shin@gmail.com>
2021-04-17 00:01:57 +0900

    행렬의 곱셈.py

--- Programmers/ETC/Level 2/행렬의 곱셈.py
+++ Programmers/ETC/Level 2/행렬의 곱셈.py
@@ -1,3 +1,6 @@
+import numpy as np
 def solution(arr1, arr2):
-    answer = [[]]
-    return answer
+    return np.array(arr1).dot(np.array(arr2)).tolist()
+
+# print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
