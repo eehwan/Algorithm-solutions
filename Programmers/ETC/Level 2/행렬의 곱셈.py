3b445599cc05ee329d00f8492fd8b4c57e4c1455
eehwan <eehwan.shin@gmail.com>
2021-04-19 17:49:16 +0900

    행렬의 곱셈

--- Programmers/ETC/Level 2/행렬의 곱셈.py
+++ Programmers/ETC/Level 2/행렬의 곱셈.py
@@ -4,10 +4,10 @@ def solution(arr1, arr2):
 
 # numpy를 이용하지 않은 풀이
 # def solution(arr1, arr2):
-#     A = len(arr1)
-#     B = len(arr1[0])
-#     C = len(arr2[0])
-#     return [[sum([arr1[idx1][idx3] * arr2[idx3][idx2] for idx3 in range(B)]) for idx2 in range(C)] for idx1 in range(A)]
+#     A = len(arr1)
+#     B = len(arr1[0])
+#     C = len(arr2[0])
+#     return [[sum([arr1[idx1][idx3] * arr2[idx3][idx2] for idx3 in range(B)]) for idx2 in range(C)] for idx1 in range(A)]
 
 # print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
 print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
