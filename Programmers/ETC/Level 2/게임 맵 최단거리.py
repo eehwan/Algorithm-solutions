7248aea17bfb2d3956794529ad1eab979f69f5eb
eehwan <eehwan.shin@gmail.com>
2021-04-24 00:12:03 +0900

    runtime error - '게임 맵 최단거리'

--- Programmers/ETC/Level 2/게임 맵 최단거리.py
+++ Programmers/ETC/Level 2/게임 맵 최단거리.py
@@ -1,14 +1,20 @@
 def solution(maps):
     m, n = len(maps), len(maps[0])
     answer = [[float('inf') for _ in range(n)] for _ in range(m)]
-    print(answer)
+    answer[0][0] = 1
+    # print(answer)
     queue = [[0,0]]
     moves = [(0,1), (0, -1), (1, 0), (-1, 0)]
     while queue:
         x, y = queue.pop()
-        if x<0 or y<0 or x>=m or y>=n:
-            continue
-        if maps[x][y] == 0: continue
         for moveX, moveY in moves:
-            queue.append(x+moveX, y+moveY)
-    return -1 if answer[-1][-1] == 0 else answer[-1][-1]
+            newX, newY = x+moveX, y+moveY
+            if newX<0 or newY<0 or newX>=m or newY>=n: continue
+            if maps[newX][newY] == 0: continue
+
+            if answer[newX][newY] > answer[x][y] + 1:
+                answer[newX][newY] = answer[x][y] + 1
+                queue.append([newX, newY])
+    return -1 if answer[-1][-1] > m*n else answer[-1][-1]
+
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1]]))
