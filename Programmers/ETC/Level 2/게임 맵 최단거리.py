7ec17b83ecebe19ad49ca2115ac5ff7bbe435e57
eehwan <eehwan.shin@gmail.com>
2021-04-28 23:44:03 +0900

    게임 맵 최단거리 - BFS

--- Programmers/ETC/Level 2/게임 맵 최단거리.py
+++ Programmers/ETC/Level 2/게임 맵 최단거리.py
@@ -1,20 +1,20 @@
+from collections import deque
 def solution(maps):
     m, n = len(maps), len(maps[0])
     answer = [[float('inf') for _ in range(n)] for _ in range(m)]
     answer[0][0] = 1
-    # print(answer)
-    queue = [[0,0]]
+    queue = deque([[0,0]])
     moves = [(0,1), (0, -1), (1, 0), (-1, 0)]
     while queue:
-        x, y = queue.pop()
+        x, y = queue.popleft()
         for moveX, moveY in moves:
-            newX, newY = x+moveX, y+moveY
-            if newX<0 or newY<0 or newX>=m or newY>=n: continue
-            if maps[newX][newY] == 0: continue
+            nx, ny = x+moveX, y+moveY
+            if nx<0 or ny<0 or nx>=m or ny>=n: continue
+            if maps[nx][ny] == 0: continue
 
-            if answer[newX][newY] > answer[x][y] + 1:
-                answer[newX][newY] = answer[x][y] + 1
-                queue.append([newX, newY])
+            if answer[nx][ny] > answer[x][y] + 1:
+                answer[nx][ny] = answer[x][y] + 1
+                queue.append([nx, ny])
     return -1 if answer[-1][-1] > m*n else answer[-1][-1]
 
 print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1]]))
