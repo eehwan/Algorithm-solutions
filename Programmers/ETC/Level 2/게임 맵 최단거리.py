638138c8969e8959755075e3fccd8f4220f128a3
eehwan <eehwan.shin@gmail.com>
2021-04-28 23:48:12 +0900

    게임 맵 최단거리 - BFS 최적화

--- Programmers/ETC/Level 2/게임 맵 최단거리.py
+++ Programmers/ETC/Level 2/게임 맵 최단거리.py
@@ -1,7 +1,7 @@
 from collections import deque
 def solution(maps):
     m, n = len(maps), len(maps[0])
-    answer = [[float('inf') for _ in range(n)] for _ in range(m)]
+    answer = [[-1 for _ in range(n)] for _ in range(m)]
     answer[0][0] = 1
     queue = deque([[0,0]])
     moves = [(0,1), (0, -1), (1, 0), (-1, 0)]
@@ -12,9 +12,10 @@ def solution(maps):
             if nx<0 or ny<0 or nx>=m or ny>=n: continue
             if maps[nx][ny] == 0: continue
 
-            if answer[nx][ny] > answer[x][y] + 1:
+            if answer[nx][ny] == -1:
                 answer[nx][ny] = answer[x][y] + 1
                 queue.append([nx, ny])
-    return -1 if answer[-1][-1] > m*n else answer[-1][-1]
+            
+    return answer[-1][-1]
 
 print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1]]))
