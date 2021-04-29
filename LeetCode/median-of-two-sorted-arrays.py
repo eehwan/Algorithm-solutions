feb58a75e72a29c3e8c3ff76041ccb40f8f408ad
eehwan <eehwan.shin@gmail.com>
2021-04-29 23:50:20 +0900

    median-of-two-sorted-arrays

--- LeetCode/median-of-two-sorted-arrays.py
+++ LeetCode/median-of-two-sorted-arrays.py
@@ -1,3 +1,12 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: list, nums2: list) -> float:
-        return 0
+        newList = sorted(nums1 + nums2)
+        return len(newList)%2 != 0 and newList[len(newList)//2] or ((newList[len(newList)//2 - 1] + newList[len(newList)//2])/2)
+
+# import statistics
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: list, nums2: list) -> float:
+#         return statistics.median(nums1+nums2)
+
+
+print(Solution.findMedianSortedArrays(0, [1,2,3], [4,5,6]))
