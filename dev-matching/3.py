a3dacbb6114911f5bf4049ec05dfa8728aaeecad
eehwan <eehwan.shin@gmail.com>
2021-04-30 02:43:55 +0900

    dev-matching 3rd Question - make refer

--- dev-matching/3.py
+++ dev-matching/3.py
@@ -1,18 +1,14 @@
 def solution(enroll, referral, seller, amount):
     incomeMap = {x: 0 for x in enroll}
-    # print("incomeMap", incomeMap)
+    refer = {x: y for x, y in zip(enroll, referral)}
     for i, salesman in enumerate(seller):
-        # print("-------------------------------")
         firstIncome = amount[i] * 100
         while salesman != "-":
             tax = int(firstIncome/10)
-            # print(salesman, firstIncome, tax)
             incomeMap[salesman] += (firstIncome - tax)
             
-            salesman = referral[enroll.index(salesman)]
+            salesman = refer[salesman]
             firstIncome = tax
-            # print(salesman, firstIncome)
-            # print(incomeMap)
     return list(incomeMap.values())
 
 print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],\
