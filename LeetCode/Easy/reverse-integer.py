a24ea14a022907601a5b1939639eb85a4049df5b
eehwan <eehwan.shin@gmail.com>
2021-06-27 00:37:05 +0900

    reverse-integer

---
+++ LeetCode/Easy/reverse-integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if 0 <= x:
+            answer = int(str(x)[::-1])
+        else:
+            answer = -int(str(x)[-1:0:-1])
+        if answer >= 2**31-1 or answer <= -2**31: return 0
+        else: return answer
