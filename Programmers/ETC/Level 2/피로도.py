6227b62d7ba38e657f346f3a950a85a81eac27cd
eehwan <eehwan.shin@gmail.com>
2022-07-11 23:45:11 +0900

    피로도 - 다른 풀이 추가

--- Programmers/ETC/Level 2/피로도.py
+++ Programmers/ETC/Level 2/피로도.py
@@ -1,3 +1,4 @@
+# 재귀를 이용한 풀이
 from copy import deepcopy
 def solution(k, dungeons, n=0):
     if len(dungeons) == 0: return n
@@ -6,6 +7,18 @@ def solution(k, dungeons, n=0):
         _dungeons = deepcopy(dungeons)
         x = _dungeons.pop(i)
         require, spend = x
-        print(k, x, _dungeons, n+1 if (k >= require) else n, sep=" | ")
         answers.append(solution(k-spend, _dungeons, n+1 if (k >= require) else n))
+    return max(answers)
+    
+# permutations를 이용한 풀이
+from itertools import permutations
+def solution(k, dungeons):
+    answers = []
+    for l in permutations(dungeons, len(dungeons)):
+        remain, result = k, 0
+        for a,b in l:
+            if(remain >= a):
+                remain -= b
+                result += 1
+        answers.append(result)
     return max(answers)
