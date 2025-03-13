0bc4c7f46e8c5de35388400215b5c1a80f0288ea
eehwan <eehwan.shin@gmail.com>
2025-03-14 00:27:18 +0900

    로직 수정

--- Programmers/완전탐색/전력망을 둘로 나누기.py
+++ Programmers/완전탐색/전력망을 둘로 나누기.py
@@ -1,17 +1,38 @@
+from collections import deque
+
+def getGroups(n, wires):    
+    graph = { i : set() for i in range(1, n+1) }
+    
+    for x, y in wires:
+        graph[x].add(y)
+        graph[y].add(x)
+    
+    visited = set()
+    groupCntList = []
+    for point in graph:
+        while point not in visited:
+            queue = deque([point])
+            group = set()
+            
+            while queue:
+                nextPoint = queue.popleft()
+                if nextPoint not in visited:
+                    visited.add(nextPoint)
+                    group.add(nextPoint)
+                    queue.extend(graph[nextPoint] - visited)
+            
+            groupCntList.append(len(group))
+
+    return groupCntList
+
 def solution(n, wires):
-    
-    def getMaxConnectionCnt(wires):
-        startPoint = wires[0]
-        connected = { startPoint[0], startPoint[1]}
-        for x, y in wires[1:]:
-            if (x in connected): connected.add(y)
-            if (y in connected): connected.add(x)
-        return len(connected)
-    
-    connectionCnts = []
+    answers = []
     for i in range(n-1):
         newWires = [v for idx, v in enumerate(wires) if idx != i]
-        cnt = getMaxConnectionCnt(newWires)
-        connectionCnts.append(cnt)
+        cntList = getGroups(n, newWires)
+
+        answers.append(abs(2 * max(cntList) - n))
     
-    return min(map(lambda x: abs(n - 2 * x), connectionCnts))
+    return min(answers)
+
+solution(12, [[1, 5], [2, 5], [3, 6], [3, 7], [3, 12], [4, 5], [4, 7], [4, 10], [8, 9], [9, 10], [11, 12]])
