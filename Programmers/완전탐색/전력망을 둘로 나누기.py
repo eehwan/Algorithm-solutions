be1819811f427abaf304f0692e7ef03f04e99f5f
eehwan <eehwan.shin@gmail.com>
2025-03-14 00:40:57 +0900

    리팩토링

--- Programmers/완전탐색/전력망을 둘로 나누기.py
+++ Programmers/완전탐색/전력망을 둘로 나누기.py
@@ -1,38 +1,39 @@
 from collections import deque
 
-def getGroups(n, wires):    
-    graph = { i : set() for i in range(1, n+1) }
-    
+def get_group_sizes(n, wires):
+    """전선 정보에서 연결된 두 그룹의 크기를 찾아 반환"""
+    graph = {i: set() for i in range(1, n + 1)}
     for x, y in wires:
         graph[x].add(y)
         graph[y].add(x)
-    
-    visited = set()
-    groupCntList = []
-    for point in graph:
-        while point not in visited:
-            queue = deque([point])
-            group = set()
-            
-            while queue:
-                nextPoint = queue.popleft()
-                if nextPoint not in visited:
-                    visited.add(nextPoint)
-                    group.add(nextPoint)
-                    queue.extend(graph[nextPoint] - visited)
-            
-            groupCntList.append(len(group))
 
-    return groupCntList
+    visited = set()
+    group_sizes = []
+
+    for node in graph:
+        if node not in visited:
+            queue = deque([node])
+            group_size = 0
+
+            while queue:
+                current = queue.popleft()
+                if current not in visited:
+                    visited.add(current)
+                    group_size += 1
+                    queue.extend(graph[current] - visited)
+
+            group_sizes.append(group_size)
+
+    return group_sizes  # 두 그룹의 크기 리스트 반환
 
 def solution(n, wires):
-    answers = []
-    for i in range(n-1):
-        newWires = [v for idx, v in enumerate(wires) if idx != i]
-        cntList = getGroups(n, newWires)
+    """전선 하나를 제거했을 때 두 그룹의 크기 차이가 최소가 되는 값을 반환"""
+    answer = float('inf')
 
-        answers.append(abs(2 * max(cntList) - n))
-    
-    return min(answers)
+    for i in range(n - 1):
+        new_wires = [w for j, w in enumerate(wires) if j != i]  # i번째 전선 제거
+        group_sizes = get_group_sizes(n, new_wires)
+        difference = abs(group_sizes[0] - group_sizes[1])
+        answer = min(difference, answer)
 
-solution(12, [[1, 5], [2, 5], [3, 6], [3, 7], [3, 12], [4, 5], [4, 7], [4, 10], [8, 9], [9, 10], [11, 12]])
+    return answer
