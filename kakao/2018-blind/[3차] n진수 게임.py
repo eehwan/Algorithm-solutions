f2905f969f9c81253b72afa89cc824b756cd3e4c
eehwan <eehwan.shin@gmail.com>
2025-03-22 21:09:57 +0900

    함수 최적화

--- kakao/2018-blind/[3차] n진수 게임.py
+++ kakao/2018-blind/[3차] n진수 게임.py
@@ -1,26 +1,33 @@
-def solution(n, t, m, p):
-    def to_base(x, n):
-        if x == 0:
-            return '0'
-        digits = '0123456789ABCDEF'
-        res = ''
-        while x > 0:
-            res = digits[x % n] + res
-            x //= n
-        return res
-
-    curr_num = 0
-    curr_idx = 0
-    answer = ''
+def to_base(x, n):
+    if x == 0:
+        return '0'
     
-    while len(answer) < t:
+    digits = '0123456789ABCDEF'
+    result = []
+
+    while x > 0:
+        result.append(digits[x % n])
+        x //= n
+    
+    return ''.join(reversed(result))
+
+
+def solution(n, t, m, p):
+    result = []
+    curr_num = 0
+    idx = 0
+
+    while len(result) < t:
         for char in to_base(curr_num, n):
-            if (curr_idx % m == p-1):
-                answer += char
-            curr_idx += 1
+            if idx % m == p - 1:
+                result.append(char)
+                if len(result) == t:
+                    break
+            idx += 1
         curr_num += 1
     
-    return answer[:t]
+    return ''.join(result)
+
 
 if __name__ == '__main__':
     samples = [
@@ -28,5 +35,5 @@ if __name__ == '__main__':
         (16, 16, 2, 1),     # "02468ACE11111111"
         (16, 16, 2, 2),     # "13579BDF01234567"
     ]
-    results = [solution(n, t, m, p) for n, t, m, p in samples]
+    results = [solution(*x) for x in samples]
     print(*results, sep='\n')
