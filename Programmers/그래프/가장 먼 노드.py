ea7582047dcb3cd7a534a8e1f6148704a955016c
eehwan <eehwan.shin@gmail.com>
2025-05-16 18:10:27 +0900

    가장 먼 노드 - 알고리즘 수정

--- Programmers/그래프/가장 먼 노드.py
+++ Programmers/그래프/가장 먼 노드.py
@@ -1,22 +1,29 @@
-def solution(n, edge):
-    queue, queue2 = [1], set()
-    node = {key:[] for key in range(1,n+1)}
-    for a, b in edge:
-        node[a].append(b)
-        node[b].append(a)
-    visited = {key:0 for key in range(2, n+1)}
-    visited[1] = 1
-    while queue:
-        for current in queue:
-            print(queue)
-            for destination in node[current]:
-                if visited[destination] == 0:
-                    queue2.add(destination)
-                    visited[destination] = 1
-                print(current, destination, visited, queue2)
-        if queue2 == set():
-            break
-        queue, queue2 = list(queue2), set()
-    return len(queue)
+from collections import deque
 
-print(solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
+def solution(n, edge):
+    graph = {key:[] for key in range(1,n+1)}
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+
+    visited = {key: False for key in range(2, n+1)}
+    visited[1] = True
+
+    queue = deque([1])
+    answer = 1      # 거리가 현재 길이인 노드의 갯수
+    while queue:
+        answer = len(queue)
+        for _ in range(answer):
+            node = queue.popleft()
+            for next_node in graph[node]:
+                if not visited[next_node]:
+                    visited[next_node] = True
+                    queue.append(next_node)
+
+    return answer
+
+if __name__ == "__main__":
+    test_cases = [
+        [6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]],  # 3
+    ]
+    print(*map(lambda x: solution(*x), test_cases), sep="\n")
