117ea8aeb3c3f58f4d01a02f16a24fe18a114fc5
eehwan <eehwan.shin@gmail.com>
2025-06-03 23:58:47 +0900

    괄호 변환 - 문제 풀이

--- kakao/2020-recuit/괄호 변환.py
+++ kakao/2020-recuit/괄호 변환.py
@@ -1,6 +1,38 @@
 def solution(p):
-    answer = 0
-    return answer
+    def isCorrect(x):
+        if (x.startswith("(")):
+            cnt = 0
+            for i, v in enumerate(x):
+                if v == "(":
+                    cnt += 1
+                else:
+                    cnt -= 1
+
+                if cnt < 0:
+                    return False
+                if cnt == 0 and len(x) - 1 == i:
+                    return True
+
+        return False
+    
+    def change(x):
+        if x == "":
+            return x
+        else:
+            cnt = 0
+            for i, v in enumerate(x):
+                if v == "(":
+                    cnt += 1
+                else:
+                    cnt -= 1
+
+                if cnt == 0:
+                    a, b = x[:i + 1], x[i + 1:]
+                    if not isCorrect(a):
+                        a = "(" + a[1:][:-1].replace("(", ":").replace(")", "(").replace(":", ")") + ")"
+                    return a + change(b)
+
+    return change(p)
 
 if __name__ == "__main__":
     test_cases = [
