6850ffc2e1ae386caabb52b9521d191042449c83
eehwan <eehwan.shin@gmail.com>
2025-06-04 04:55:38 +0900

    괄호 변환 - 코드 정리

--- kakao/2020-recuit/괄호 변환.py
+++ kakao/2020-recuit/괄호 변환.py
@@ -1,41 +1,36 @@
 def solution(p):
-    def isCorrect(x):
-        if (x.startswith("(")):
-            cnt = 0
-            for i, v in enumerate(x):
-                if v == "(":
-                    cnt += 1
-                else:
-                    cnt -= 1
-
-                if cnt < 0:
-                    return False
-            if cnt == 0:
-                return True
-
-        return False
+    def is_correct(x):
+        cnt = 0
+        for char in x:
+            if char == "(":
+                cnt += 1
+            else:
+                cnt -= 1
+            if cnt < 0:
+                break
+        return cnt == 0
     
-    def change(x):
-        if x == "":
-            return x
-        else:
-            cnt = 0
-            for i, v in enumerate(x):
-                if v == "(":
-                    cnt += 1
+    def transform(s):
+        if not s:
+            return ""
+        
+        cnt = 0
+        for i, char in enumerate(s):
+            if char == "(":
+                cnt += 1
+            else:
+                cnt -= 1
+        
+            if cnt == 0:
+                u, v = s[:i + 1], s[i + 1:]
+                
+                if is_correct(u):
+                    return u + transform(v)
                 else:
-                    cnt -= 1
+                    new_u = ''.join(')' if c == '(' else '(' for c in u[1:-1])
+                    return '(' + transform(v) + ')' + new_u
 
-                if cnt == 0:
-                    a, b = x[:i + 1], x[i + 1:]
-                    
-                    if isCorrect(a):
-                        return a + change(b)
-                    else:
-                        a = a[1:][:-1].replace("(", ":").replace(")", "(").replace(":", ")")
-                        return f'({change(b)}){a}'
-
-    return change(p)
+    return transform(p)
 
 if __name__ == "__main__":
     test_cases = [
