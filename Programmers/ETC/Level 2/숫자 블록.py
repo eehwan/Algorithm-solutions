116cf68fbbad343e674c455b56a47f5278ddf897
eehwan <eehwan.shin@gmail.com>
2025-06-22 00:15:24 +0900

    숫자 블록 - 문제 해결

--- Programmers/ETC/Level 2/숫자 블록.py
+++ Programmers/ETC/Level 2/숫자 블록.py
@@ -4,16 +4,20 @@ def solution(begin, end):
         if i == 1:
             result.append(0)
         else:
-            v = 1
-            for j in range(2, i // 2 + 1):
+            limit = min(10000000, i // 2, int(i ** 0.5))
+            temp = [1]
+            for j in range(limit, 1, -1):
                 if i % j == 0:
-                    v = i // j
-            result.append(v)
+                    temp += filter(lambda x: x <= 10000000, [j, i // j])
+            result.append(max(temp))
     return result
 
 
 if __name__ == "__main__":
     test_cases = [
-        [1, 10],   # [0, 1, 1, 2, 1, 3, 1, 4, 3, 5]
+        [1, 10],                    # [0, 1, 1, 2, 1, 3, 1, 4, 3, 5]
+        [477559014, 477559014],     # [6]
+        [1000000000, 1000000000],   # [10000000]
+        [100000007, 100000007],     # [1]
     ]
     print(*map(lambda x: solution(*x), test_cases), sep="\n")
