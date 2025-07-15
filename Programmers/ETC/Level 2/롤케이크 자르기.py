0f16a5bea35410fdf85b216ab9fb2f7a9bae2ecf
eehwan <eehwan.shin@gmail.com>
2025-07-15 19:28:37 +0900

    롤케이크 자르기-문제 해결

--- Programmers/ETC/Level 2/롤케이크 자르기.py
+++ Programmers/ETC/Level 2/롤케이크 자르기.py
@@ -1,5 +1,23 @@
+from collections import Counter
+
 def solution(topping):
-    answer = -1
+    answer = 0
+
+    left, right = Counter(), Counter(topping)
+    left_types, right_types = 0,  len(right)
+
+    for t in topping:
+        if left[t] == 0:
+            left_types += 1
+        left[t] += 1
+
+        right[t] -= 1
+        if right[t] == 0:
+            right_types -= 1
+
+        if left_types == right_types:
+            answer += 1
+
     return answer
 
 if __name__ == "__main__":
